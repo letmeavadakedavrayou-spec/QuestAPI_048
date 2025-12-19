eb0d35628563628671d263e89ea11c4a973c9e57
nachool <nachma.olivia1394@gmail.com>
2025-12-19 23:19:05 +0700

    memuat view model untuk halaman entry

--- app/src/main/java/com/example/questapi_048/uicontroller/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questapi_048/uicontroller/viewmodel/EntryViewModel.kt
@@ -1,4 +1,42 @@
 package com.example.questapi_048.uicontroller.viewmodel
 
-class EntryViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.questapi_048.modeldata.DetailSiswa
+import com.example.questapi_048.modeldata.UIStateSiswa
+import com.example.questapi_048.modeldata.toDataSiswa
+import com.example.questapi_048.repositori.RepositoryDataSiswa
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    // Fungsi untuk menangani saat ada perubahan pada text input
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            val sip = repositoryDataSiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+            if (sip.isSuccessful) {
+                println("Sukses Tambah Data : ${sip.message()}")
+            } else {
+                println("Gagal tambah data : ${sip.errorBody()}")
+            }
+        }
+    }
 }
