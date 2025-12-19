0f2ccf10e7ae67722d4b4852b59e98c929ade9b0
nachool <nachma.olivia1394@gmail.com>
2025-12-19 23:15:55 +0700

    nambah lib retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -57,6 +58,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging)
+    implementation(libs.retrofit.core)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
