126d8b3de51b014f4d430116daa59daacff6f9b7
nachool <nachma.olivia1394@gmail.com>
2025-12-19 23:16:43 +0700

    menambahkan ServiceApiSiswa.kt

--- app/src/main/java/com/example/questapi_048/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_048/apiservice/ServiceApiSiswa.kt
@@ -1,4 +1,18 @@
 package com.example.questapi_048.apiservice
 
-class ServiceApiSiswa {
+
+import com.example.questapi_048.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface ServiceApiSiswa {
+    @GET(value = "bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @POST(value = "insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
 }
