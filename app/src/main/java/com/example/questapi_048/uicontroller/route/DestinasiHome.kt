b30bff98a82b6dab6ef73fd1157437063a6b3250
nachool <nachma.olivia1394@gmail.com>
2025-12-19 23:20:25 +0700

    memuat destinasi untuk  home

--- app/src/main/java/com/example/questapi_048/uicontroller/route/DestinasiHome.kt
+++ app/src/main/java/com/example/questapi_048/uicontroller/route/DestinasiHome.kt
@@ -1,4 +1,8 @@
 package com.example.questapi_048.uicontroller.route
 
-class DestinasiHome {
+import com.example.questapi_048.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
