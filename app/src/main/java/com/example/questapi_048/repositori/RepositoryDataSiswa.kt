d5c264356364767dceb36b1e3881f65ba6dc6926
nachool <nachma.olivia1394@gmail.com>
2025-12-26 23:01:37 +0700

    menambahkan repositori untuk delete dan update

--- app/src/main/java/com/example/questapi_048/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_048/repositori/RepositoryDataSiswa.kt
@@ -7,6 +7,9 @@ import com.example.questapi_048.modeldata.DataSiswa
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun updateSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun deleteSiswa(id: Int): retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
@@ -14,4 +17,7 @@ class JaringanRepositoryDataSiswa(
 ) : RepositoryDataSiswa {
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun updateSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun deleteSiswa(id: Int): retrofit2.Response<Void> = serviceApiSiswa.hapusSatuSiswa(id)
 }
