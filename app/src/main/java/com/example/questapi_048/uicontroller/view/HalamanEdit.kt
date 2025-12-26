198b270529fb681b5f3ce709354edc502ea189e9
nachool <nachma.olivia1394@gmail.com>
2025-12-26 23:05:18 +0700

    menambah view HalamanEdit.kt

--- app/src/main/java/com/example/questapi_048/uicontroller/view/HalamanEdit.kt
+++ app/src/main/java/com/example/questapi_048/uicontroller/view/HalamanEdit.kt
@@ -1,2 +1,45 @@
 package com.example.questapi_048.uicontroller.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_048.uicontroller.viewmodel.EditViewModel
+import com.example.questapi_048.uicontroller.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemEditScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = "Edit Siswa",
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        }
+    ) { innerPadding ->
+        EntryBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
