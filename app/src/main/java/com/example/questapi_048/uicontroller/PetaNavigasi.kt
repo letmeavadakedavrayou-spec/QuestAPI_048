6386e5a1cd9c7a90cb643ebb62034800fc58cdb9
nachool <nachma.olivia1394@gmail.com>
2025-12-26 23:02:34 +0700

    menambahkan navigasi edit & detail

--- app/src/main/java/com/example/questapi_048/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_048/uicontroller/PetaNavigasi.kt
@@ -3,13 +3,19 @@ package com.example.questapi_048.uicontroller
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.questapi_048.uicontroller.route.DestinasiDetail
+import com.example.questapi_048.uicontroller.route.DestinasiEdit
 import com.example.questapi_048.uicontroller.route.DestinasiEntry
 import com.example.questapi_048.uicontroller.route.DestinasiHome
+import com.example.questapi_048.uicontroller.view.DetailScreen
 import com.example.questapi_048.uicontroller.view.EntrySiswaScreen
 import com.example.questapi_048.uicontroller.view.HomeScreen
+import com.example.questapi_048.uicontroller.view.ItemEditScreen
 
 @Composable
 fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier = Modifier) {
@@ -30,8 +36,7 @@ fun HostNavigasi(
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
                 onDetailClick = { itemId ->
-                    // navController.navigate("${DestinasiDetail.route}/$itemId")
-                    // Implementasi detail nanti jika dibutuhkan
+                    navController.navigate("${DestinasiDetail.route}/$itemId")
                 }
             )
         }
@@ -42,5 +47,28 @@ fun HostNavigasi(
                 } }
             )
         }
+
+        composable(
+            route = DestinasiDetail.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiDetail.idSiswa) { type = NavType.IntType })
+        ) {
+            DetailScreen(
+                navigateBack = { navController.popBackStack() },
+                navigateToEditItem = { itemId ->
+                    navController.navigate("${DestinasiEdit.route}/$itemId")
+                }
+            )
+        }
+
+        composable(
+            route = DestinasiEdit.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiEdit.idSiswa) { type = NavType.IntType })
+        ) {
+            ItemEditScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
+            )
+        }
+
     }
 }
